046d919b3a55c904e25575153e764a1819650b5f
Kevin (Pololu) <dev-kevin@pololu.com>
2011-11-09 17:51:43 -0800

    added new definitions to LSM303/LSM303.h

--- LSM303/LSM303.h
+++ LSM303/LSM303.h
@@ -3,6 +3,19 @@
 
 #include <WProgram.h> // for byte data type
 
+// device types
+
+#define LSM303_DEVICE_DLH  0
+#define LSM303_DEVICE_DLM  1
+#define LSM303_DEVICE_DLHC 2
+#define LSM303_DEVICE_AUTO 3
+
+// SA0_A states
+
+#define LSM303_SA0_A_LOW  0
+#define LSM303_SA0_A_HIGH 1
+#define LSM303_SA0_A_AUTO 2
+
 // register addresses
 
 #define LSM303_CTRL_REG1_A       0x20
@@ -10,7 +23,8 @@
 #define LSM303_CTRL_REG3_A       0x22
 #define LSM303_CTRL_REG4_A       0x23
 #define LSM303_CTRL_REG5_A       0x24
-#define LSM303_HP_FILTER_RESET_A 0x25
+#define LSM303_CTRL_REG6_A       0x25 // DLHC only
+#define LSM303_HP_FILTER_RESET_A 0x25 // DLH, DLM only
 #define LSM303_REFERENCE_A       0x26
 #define LSM303_STATUS_REG_A      0x27
 
@@ -21,6 +35,9 @@
 #define LSM303_OUT_Z_L_A         0x2C
 #define LSM303_OUT_Z_H_A         0x2D
 
+#define LSM303_FIFO_CTRL_REG_A   0x2E // DLHC only
+#define LSM303_FIFO_SRC_REG_A    0x2F // DLHC only
+
 #define LSM303_INT1_CFG_A        0x30
 #define LSM303_INT1_SRC_A        0x31
 #define LSM303_INT1_THS_A        0x32
@@ -30,33 +47,48 @@
 #define LSM303_INT2_THS_A        0x36
 #define LSM303_INT2_DURATION_A   0x37
 
+#define LSM303_CLICK_CFG_A       0x38 // DLHC only
+#define LSM303_CLICK_SRC_A       0x39 // DLHC only
+#define LSM303_CLICK_THS_A       0x3A // DLHC only
+#define LSM303_TIME_LIMIT_A      0x3B // DLHC only
+#define LSM303_TIME_LATENCY_A    0x3C // DLHC only
+#define LSM303_TIME_WINDOW_A     0x3D // DLHC only
+
 #define LSM303_CRA_REG_M         0x00
 #define LSM303_CRB_REG_M         0x01
 #define LSM303_MR_REG_M          0x02
 
 #define LSM303_OUT_X_H_M         0x03
 #define LSM303_OUT_X_L_M         0x04
-#define LSM303_OUT_Y_H_M         -1
-#define LSM303_OUT_Y_L_M         -2
-#define LSM303_OUT_Z_H_M         -3
-#define LSM303_OUT_Z_L_M         -4
+#define LSM303_OUT_Y_H_M         -1   // The addresses of the Y and Z magnetometer output registers 
+#define LSM303_OUT_Y_L_M         -2   // are reversed on the DLM and DLHC relative to the DLH.
+#define LSM303_OUT_Z_H_M         -3   // These four defines have dummy values so the library can 
+#define LSM303_OUT_Z_L_M         -4   // determine the correct address based on the device type.
 
 #define LSM303_SR_REG_M          0x09
 #define LSM303_IRA_REG_M         0x0A
 #define LSM303_IRB_REG_M         0x0B
 #define LSM303_IRC_REG_M         0x0C
 
-// Y and Z are in order after X on LSM303DLH
+#define LSM303_WHO_AM_I_M        0x0F // DLM only
+
+#define LSM303_TEMP_OUT_H_M      0x31 // DLHC only
+#define LSM303_TEMP_OUT_L_M      0x32 // DLHC only
+
 #define LSM303DLH_OUT_Y_H_M      0x05
 #define LSM303DLH_OUT_Y_L_M      0x06
 #define LSM303DLH_OUT_Z_H_M      0x07
 #define LSM303DLH_OUT_Z_L_M      0x08
 
-// Z and Y registers are swapped on LSM303DLM
 #define LSM303DLM_OUT_Z_H_M      0x05
 #define LSM303DLM_OUT_Z_L_M      0x06
 #define LSM303DLM_OUT_Y_H_M      0x07
 #define LSM303DLM_OUT_Y_L_M      0x08
+
+#define LSM303DLHC_OUT_Z_H_M     0x05
+#define LSM303DLHC_OUT_Z_L_M     0x06
+#define LSM303DLHC_OUT_Y_H_M     0x07
+#define LSM303DLHC_OUT_Y_L_M     0x08
 
 class LSM303
 {
@@ -73,14 +105,14 @@ class LSM303
 	
 		LSM303(void);
 		
-		void init(void);
+		void init(byte device = LSM303_DEVICE_AUTO, byte sa0_a = LSM303_SA0_A_AUTO);
 		
 		void enableDefault(void);
 		
 		void writeAccReg(byte reg, byte value);
 		byte readAccReg(byte reg);
 		void writeMagReg(byte reg, byte value);
-		byte readMagReg(byte reg);
+		byte readMagReg(int reg);
 		
 		void readAcc(void);
 		void readMag(void);
@@ -93,6 +125,10 @@ class LSM303
 		static void vector_cross(const vector *a, const vector *b, vector *out);
 		static float vector_dot(const vector *a,const vector *b);
 		static void vector_normalize(vector *a);
+		
+	private:
+		byte _device; // chip type (DLH, DLM, or DLHC)
+		byte acc_address;
 };
 
 #endif
