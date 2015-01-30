57ab23164d35af13cc000e1a2db470620b532b9e
Kevin (Pololu) <dev-kevin@pololu.com>
2015-01-29 18:37:03 -0800

    moved vector function templates to header file so they can be used outside of LSM303.cpp

--- LSM303/LSM303.cpp
+++ LSM303/LSM303.cpp
@@ -497,54 +497,6 @@ float LSM303::heading(void)
   }
 }
 
-/*
-Returns the angular difference in the horizontal plane between the
-"from" vector and north, in degrees.
-
-Description of heading algorithm:
-Shift and scale the magnetic reading based on calibration data to find
-the North vector. Use the acceleration readings to determine the Up
-vector (gravity is measured as an upward acceleration). The cross
-product of North and Up vectors is East. The vectors East and North
-form a basis for the horizontal plane. The From vector is projected
-into the horizontal plane and the angle between the projected vector
-and horizontal north is returned.
-*/
-template <typename T> float LSM303::heading(vector<T> from)
-{
-    vector<int32_t> temp_m = {m.x, m.y, m.z};
-
-    // subtract offset (average of min and max) from magnetometer readings
-    temp_m.x -= ((int32_t)m_min.x + m_max.x) / 2;
-    temp_m.y -= ((int32_t)m_min.y + m_max.y) / 2;
-    temp_m.z -= ((int32_t)m_min.z + m_max.z) / 2;
-
-    // compute E and N
-    vector<float> E;
-    vector<float> N;
-    vector_cross(&temp_m, &a, &E);
-    vector_normalize(&E);
-    vector_cross(&a, &E, &N);
-    vector_normalize(&N);
-
-    // compute heading
-    float heading = atan2(vector_dot(&E, &from), vector_dot(&N, &from)) * 180 / M_PI;
-    if (heading < 0) heading += 360;
-    return heading;
-}
-
-template <typename Ta, typename Tb, typename To> void LSM303::vector_cross(const vector<Ta> *a,const vector<Tb> *b, vector<To> *out)
-{
-  out->x = (a->y * b->z) - (a->z * b->y);
-  out->y = (a->z * b->x) - (a->x * b->z);
-  out->z = (a->x * b->y) - (a->y * b->x);
-}
-
-template <typename Ta, typename Tb> float LSM303::vector_dot(const vector<Ta> *a, const vector<Tb> *b)
-{
-  return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
-}
-
 void LSM303::vector_normalize(vector<float> *a)
 {
   float mag = sqrt(vector_dot(a, a));
